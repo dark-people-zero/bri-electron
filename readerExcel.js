6f74d3bbc76d2238eec89c54bb392a8faed382e9
Dark People <dark.people.zero@gmail.com>
2022-12-30 06:01:45 +0700

    memperbaiki reader excel

--- readerExcel.js
+++ readerExcel.js
@@ -27,9 +27,10 @@ const readerExcel = (dir) => {
         }
     
         if (e.name == "Sheet2") {
-            var key = data[0];
+            var key = [ 'tanggal', 'transaksi', 'debet', 'kredit', 'saldo' ];
             var saldoAwal = data[1];
             var saldoAkhir = data.at(-2);
+            console.log(data);
             hasil.mutasi = data.slice(2,-3).map(e => {
                 var res = {};
                 key.forEach((val, ind) => {
